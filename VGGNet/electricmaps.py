b3e2999c8b3262f6f2bf3aca2d2ddbea54b77d6e
wonseok <98411696+FarmingWon@users.noreply.github.com>
2025-12-19 11:26:28 +0900

    VGGNet Demo Files

---
+++ VGGNet/electricmaps.py
@@ -0,0 +1,19 @@
+import asyncio
+from aioelectricitymaps import ElectricityMaps, ZoneRequest # Python 3.11
+# from aioelectricitymaps import ElectricityMaps  # Python 3.8
+import pandas as pd
+
+class electric:
+    def __init__(self, token):
+        self.token = token
+        
+    async def get_carbon_intensity(self,region):
+        async with ElectricityMaps(token=self.token) as em:
+            # response = await em.latest_carbon_intensity_by_country_code(region) # Docker Python Version issue, Python 3.8 기준 
+            # return float(response.data.carbon_intensity)
+            response = await em.latest_carbon_intensity(ZoneRequest(region)) # Python 3.11 
+            return float(response.carbon_intensity)
+            # print(f"Carbon intensity in {region_full}: {response.carbon_intensity} gCO2eq/kWh")
+            # print(response.timestamp)            
+            # IE : Ireland 
+        
